72312901ef1d75f649f313fbac8fc9104fa66f11
taiki-umetsu <taiki.umetsu.st@gmail.com>
2020-04-08 21:09:39 +0900

    Change routing about users to resourceful one

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  root 'application#hello'
+  resources :users
+  root 'users#index'
 end
