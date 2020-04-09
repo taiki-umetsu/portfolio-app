a9f90259000f4996e494db3526b472fdd6ed47cd
taiki-umetsu <taiki.umetsu.st@gmail.com>
2020-04-09 10:45:15 +0900

    Modify the style of the flash messages

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def bootstrap_alert(key)
+    case key
+    when 'alert'
+      'warning'
+    when 'notice'
+      'success'
+    when 'error'
+      'danger'
+    end
+  end
 end
