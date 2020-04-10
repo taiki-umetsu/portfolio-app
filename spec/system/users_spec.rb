36a1e962365c8a1008b8a085cc090a02f2641f90
taiki-umetsu <taiki.umetsu.st@gmail.com>
2020-04-10 12:02:59 +0900

    Add system spec about signup form

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -8,23 +8,50 @@ RSpec.describe 'Users', type: :system do
     driven_by :selenium_chrome_headless
   end
 
-  context 'when valid email and password is filled in' do
-    it ' is successful to log in ' do
-      visit new_user_session_path
-      fill_in 'メールアドレス', with: user.email
-      fill_in 'パスワード', with: user.password
-      click_button 'Log in'
-      expect(page).to have_content 'ログインしました'
+  describe 'Log in form' do
+    context 'when valid email and password is filled in' do
+      it ' is successful to log in ' do
+        visit new_user_session_path
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: user.password
+        click_button 'LOG IN'
+        expect(page).to have_content 'ログインしました'
+      end
+    end
+
+    context 'when invalid email and password is filled in' do
+      it ' fails to log in ' do
+        visit new_user_session_path
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード', with: ''
+        click_button 'LOG IN'
+        expect(page).to have_content 'メールアドレスまたはパスワードが違います。'
+      end
     end
   end
 
-  context 'when invalid email and password is filled in' do
-    it ' fails to log in ' do
-      visit new_user_session_path
-      fill_in 'メールアドレス', with: ''
-      fill_in 'パスワード', with: ''
-      click_button 'Log in'
-      expect(page).to have_content 'メールアドレスまたはパスワードが違います。'
+  describe 'Sign up form' do
+    before do
+      visit new_user_registration_path
+    end
+    context 'when valid email and password is filled in' do
+      it ' is successful to log in ' do
+        fill_in 'メールアドレス', with: 'example@example.com'
+        fill_in 'パスワード', with: 'password'
+        fill_in '確認用パスワード', with: 'password'
+        click_button 'SIGN UP'
+        expect(page).to have_content 'アカウント登録が完了しました'
+      end
+    end
+    context 'when invalid email and password is filled in' do
+      it ' fails to log in ' do
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード', with: ''
+        fill_in '確認用パスワード', with: ''
+        click_button 'SIGN UP'
+        expect(page).to have_content 'メールアドレス を記入してください'
+        expect(page).to have_content 'パスワード を記入してください'
+      end
     end
   end
 end
