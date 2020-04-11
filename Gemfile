c5f62eaab62361f8e59522c0f76cafc7da510169
taiki-umetsu <taiki.umetsu.st@gmail.com>
2020-04-11 12:43:59 +0900

    Use Amazon Rekognition

--- Gemfile
+++ Gemfile
@@ -45,6 +45,10 @@ gem 'devise-i18n'
 
 gem 'devise-i18n-views'
 
+gem 'aws-sdk', '~> 3'
+
+gem 'mini_magick'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/helpers/avatars_helper.rb
+++ app/helpers/avatars_helper.rb
@@ -1,4 +1,34 @@
 # frozen_string_literal: true
 
 module AvatarsHelper
+  def face_coordinate(photo)
+    credentials = Aws::Credentials.new(
+      ENV['RTB_ACCESS_KEY'],
+      ENV['RTB_SECRET_KEY']
+    )
+    client = Aws::Rekognition::Client.new credentials: credentials
+    attrs = {
+      image: {
+        bytes: photo
+      },
+      attributes: ['ALL']
+    }
+    eye_left_ratio = { x: 0, y: 0 }
+    eye_right_ratio = { x: 0, y: 0 }
+    nose_ratio = { x: 0, y: 0 }
+
+    response = client.detect_faces attrs
+    puts 'Detected faces for: '
+    response.face_details.each do |face_detail|
+      eye_left_ratio = { x: face_detail.landmarks[0].x,
+                         y: face_detail.landmarks[0].y }
+      eye_right_ratio = { x: face_detail.landmarks[1].x,
+                          y: face_detail.landmarks[1].y }
+      nose_ratio = { x: face_detail.landmarks[4].x,
+                     y: face_detail.landmarks[4].y }
+    end
+    puts eye_left_ratio
+    puts eye_right_ratio
+    puts nose_ratio
+  end
 end
