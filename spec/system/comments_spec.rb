f2d234df352b8cb171c4b0e226d816daa82bfde3
taiki-umetsu <taiki.umetsu.st@gmail.com>
2020-06-06 15:56:04 +0900

    Add test : comments page

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -3,10 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments', type: :system do
+  let!(:me) { create(:user) }
+  let!(:others) { create(:user) }
+  let!(:avatar) { create(:avatar, user: others, public: true) }
+  let!(:my_comment) { create(:comment, avatar: avatar, user: me, content: 'my comment') }
+  let!(:others_comment) { create(:comment, avatar: avatar, user: others, content: 'others comment') }
   describe 'create comment', js: true do
-    let!(:me) { create(:user) }
-    let!(:others) { create(:user) }
-    let!(:avatar) { create(:avatar, user: others, public: true) }
     before do
       sign_in me
       visit user_path(others)
@@ -31,29 +33,40 @@ RSpec.describe 'Comments', type: :system do
       expect(page).to_not have_css '.upload-field'
     end
   end
-  pending 'can not load comment writtten by Vue' do
+
+  describe 'comments page' do
+    before do
+      sign_in me
+      visit user_path(others)
+      sleep 0.5
+      find('.comment-counter').click
+      sleep 0.5
+    end
+    it 'goes comments page' do
+      expect(page).to have_content 'コメント一覧'
+      expect(page).to have_css '.users-wrapper'
+    end
+    it 'goes back to user page' do
+      find('#page-back').click
+      expect(page).to have_content '公開アバター'
+    end
+    it 'is comments on the page' do
+      expect(page).to have_content my_comment.content
+      expect(page).to have_content others_comment.content
+    end
+    it 'goes to commented user page' do
+      click_on me.name
+      expect(page).to have_content me.name
+      expect(page).to have_content '公開アバター'
+    end
+
     describe 'destroy comment', js: true do
-      let!(:me) { create(:user) }
-      let!(:others) { create(:user) }
-      let!(:avatar) { create(:avatar, user: others, public: true) }
-      let!(:my_comment) { create(:comment, avatar: avatar, user: me, content: 'my comment') }
-      let!(:others_comment) { create(:comment, avatar: avatar, user: others, content: 'others comment') }
-      before do
-        sign_in me
-        visit user_path(others)
-        find('div[id=avatar-comment]').click
-      end
-      it { expect(page).to have_content 'my comment' }
-      it { expect(page).to have_content 'others comment' }
-      context 'try to destroy my comment' do
-        before do
-          # click ok because confirmation form is appeared
-          accept_confirm do
-            # find the icon which has link to delete comment
-            find(".comment#{my_comment.id}").find('.fa-trash-alt').click
-          end
+      it 'destroys my comment' do
+        # confirmation form will be appeared
+        accept_confirm do
+          find(".comment#{my_comment.id}").find('.fa-trash-alt').click
         end
-        it { expect(page).to_not have_content 'my comment' }
+        expect(page).to_not have_content 'my comment'
       end
 
       context 'try to destroy others comment' do
